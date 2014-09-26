ba12cbdfea1442354c68b215915a4d9fdef9bd11
darienjh <darihayes@stcharlessd.org>
2014-09-26 09:11:17 -0500

    gnfd

--- Craps_Roller.py
+++ Craps_Roller.py
@@ -3,15 +3,16 @@
 # Original Author: Michael Dawson
 # Last Edited: Thorin Schmidt
 # Date Edited: 9/26/2014
+# Last Edited: Darien Hayes
+# Date Edited: 9/26/2014 
 
-impart random
+import random
 
 # generate random numbers 1 - 6
 die1 = random.randint(1, 6) 
 die2 = random.randrange(6)
 
-total = 'die1' + 'die2'
+total = int(die1+die2)
 
 print("You rolled a", die1, "and a", die2, "for a total of", total)
-
 input("\n\nPress the enter key to exit.")
